5b759e4e4d8070822f6789ab18d6c745f45cf791
MXFia19 <130248051+MXFia19@users.noreply.github.com>
2026-06-19 14:34:22 +0200

    Remove unnecessary comments and clean up code

--- hostcord/hostcord.js
+++ hostcord/hostcord.js
@@ -1,4 +1,3 @@
-```javascript
 // ==========================================
 // 🔓 SORA MODULE — ZXCSTREAM (CATALOGUE TMDB + CRACK)
 // ==========================================
@@ -257,5 +256,3 @@ async function soraFetch(url, options = { headers: {}, method: 'GET', body: null
         try { return await fetch(url, options); } catch { return null; }
     }
 }
-
-```
